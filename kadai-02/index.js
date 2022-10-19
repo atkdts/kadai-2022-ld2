17ac703876bb097f0f50f7617299a57e780fa9cf
atkdts <as.tkdsczor@gmail.com>
2022-10-19 04:50:23 +0000

    課題2を提出

---
+++ kadai-02/index.js
@@ -0,0 +1,16 @@
+const firstname = "Raito"
+const familyname = "Yagami"
+const fullname = firstname + familyname
+console.log(fullname)
+
+
+const num = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(num)
+
+
+const dog = true
+const cat = false
+console.log("犬または猫を飼っている" ,  dog || cat)
+console.log("犬と猫の両方を飼っている" , dog && cat)
+console.log("犬も猫も飼っていない" , !dog && !cat)
+console.log("犬または猫を飼っている、わけではない" , !(dog || cat))
---
+++ kadai-02/sample.js
@@ -0,0 +1,18 @@
+console.log('週刊少年ジャンプ')
+
+console.log('週刊少年マガジン')//これはコメントです。
+/*これもこめんと*/
+/*改行しても
+有効のまま*/
+
+//変数を定義する
+const name = 'Tama'
+const age = 80
+
+//変数の内容を出力する
+console.log(name)
+console.log(age)
+
+//テンプレート文字列
+const phrase = `こんにちは、${name}!`
+
